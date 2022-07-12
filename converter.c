0544d505c4faa27e6a2cee4802a68a709e9718ec
Chukwudi <ikenwakochukwudi@gmail.com>
2022-07-12 13:29:07 -0700

    returned the state of the files to the previous 'success' state

--- converter.c
+++ converter.c
@@ -7,17 +7,18 @@
  *
  * Return: void function pointer to function matched by symbol
  */
-void (*converter(const char *symbol))(va_list list)
+int (*converter(const char *symbol))(va_list list, char *buffer)
 {
 	formatter_t func[] = {
 		{"c", print_c},
 		{"s", print_s},
-		{"d", print_d},
-		{"i", print_i}
+		{"d", print_d_i},
+		{"i", print_d_i},
+		{"%", print_percent}
 	};
 	int i;
 
-	for (i = 0; i < 4; i++)
+	for (i = 0; i < 5; i++)
 	{
 		if (*symbol == *(func[i].symbol))
 			return (func[i].f);
@@ -25,4 +26,5 @@ void (*converter(const char *symbol))(va_list list)
 
 	write(2, "Conversion specifier lacks type at end of format", 48);
 	exit(1);
+	return (NULL);
 }
--- main.h
+++ main.h
@@ -12,39 +12,15 @@
  */
 typedef struct formatter {
 	char *symbol;
-<<<<<<< HEAD
-<<<<<<< HEAD
-	void (*f)(va_list arg_list);
-} formatter_t;
-
-int _printf(const char *format, ...);
-void (*converter(const char *))(va_list arg_list);
-void print_number(int n);
-void print_c(va_list arg_list);
-void print_s(va_list arg_list);
-void print_d(va_list arg_list);
-void print_i(va_list arg_list);
-=======
 	int (*f)(va_list arg_list, char *buffer);
 } formatter_t;
 
 int _printf(const char *format, ...);
-=======
-	int (*f)(va_list arg_list, char *buffer);
-} formatter_t;
-
-int _printf(const char *format, ...);
->>>>>>> parent of 420ccc0... Add files via upload
 int (*converter(const char *))(va_list arg_list, char *buffer);
 void print_number(int n, char *buffer);
 int print_c(va_list arg_list, char *buffer);
 int print_s(va_list arg_list, char *buffer);
-int print_d(va_list arg_list, char *buffer);
-int print_i(va_list arg_list, char *buffer);
+int print_d_i(va_list arg_list, char *buffer);
 int print_percent(va_list arg_list, char *buffer);
-<<<<<<< HEAD
->>>>>>> parent of 420ccc0... Add files via upload
-=======
->>>>>>> parent of 420ccc0... Add files via upload
 
 #endif /* _MAIN_H_ */
